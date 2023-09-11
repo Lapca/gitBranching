8a0f5fa26cb90c67daa5d40fb341ef447da1584b
Erik Fridrich <fridrich.eriik@gmail.com>
2023-09-11 10:21:38 +0200

    B1

---
+++ gitBranching/src/AgePrinter.java
@@ -0,0 +1,11 @@
+public class AgePrinter {
+    public void count(int age) {
+        System.out.println("My age is: " + age);
+    }
+
+    public static void main(String[] args) {
+        AgePrinter printer = new AgePrinter();
+
+        printer.count(25);
+    }
+}
