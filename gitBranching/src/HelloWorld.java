4b07297194f21bcae3c5ff5fad4d8131919cdbe6
Martin Lapčík <xlapcik@fi.muni.cz>
2023-09-11 14:48:49 +0200

    a6

--- gitBranching/src/HelloWorld.java
+++ gitBranching/src/HelloWorld.java
@@ -1,7 +1,10 @@
 public class HelloWorld {
     public void greetingsToWorld(String name) {
-
-        System.out.println("Hello World from " + name);
+        System.out.println("erik greet\n" +
+                "martin helloworld\n" +
+                "pavel future\n" +
+                "laci cheer\n" +
+                "gusztav count");
 
     }
 }
