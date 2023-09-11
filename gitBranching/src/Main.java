ade096ef10f8e5bbd7680a64518432d4f5daecf8
Laszlo Fodor <fodor.zatureczki@gmail.com>
2023-09-11 11:48:01 +0200

    d3

--- gitBranching/src/Main.java
+++ gitBranching/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         AgePrinter agePrinter = new AgePrinter();
         agePrinter.count(25);
         HelloWorld helloWorld = new HelloWorld();
-        helloWorld.greetingsToWorld("Pavel");
+        helloWorld.greetingsToWorld("!!Pavel!!");
 
         Cheerer cheerer = new Cheerer();
         cheerer.cheer("Slovak!");
