c1b0e52ec65fa20094f0828979b44f2ba4ec54dd
czimreg <czimreg@gmail.com>
2023-09-11 13:45:46 +0100

    c6.3

--- gitBranching/src/AgePrinter.java
+++ gitBranching/src/AgePrinter.java
@@ -1,13 +1,7 @@
 public class AgePrinter {
     public void count(int age) {
-        System.out.println("1");
+        System.out.println("Counting the countless " + age);
     }
-
-//    public static void main(String[] args) {
-//        AgePrinter printer = new AgePrinter();
-//
-//        printer.count(25);
-//    }
 
     public AgePrinter() {
     }
